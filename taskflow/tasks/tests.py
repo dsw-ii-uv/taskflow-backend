a9c04699f7f910d2951cf8c5a28a724c8a6ad876
David2105 <71578943+DavidReyesCuartas@users.noreply.github.com>
2025-09-07 16:49:20 -0500

    Feature/unit test (#3)
    * Add unit tests for Task and User models
    
    * Add task model fields and update serializers for priority and state management

--- taskflow/tasks/tests.py
+++ taskflow/tasks/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from tasks.models import Task
+from users.models import User
 
-# Create your tests here.
+
+class TaskTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(
+            first_name="John",
+            last_name="Doe",
+            email="john123@example.com",
+            department=1,
+            is_boss=False,
+        )
+
+        Task.objects.create(
+            title="Task 1",
+            description="Description for Task 1",
+            created_at="2025-09-08T15:20:00",
+            due_date="2025-09-10T15:20:00",
+            completed=False,
+            posted_by_id=1
+        )
+        
+        Task.objects.create(
+            title="Task 2",
+            description="Description for Task 2",
+            created_at="2025-09-08T15:20:00",
+            due_date="2025-09-11T15:20:00",
+            completed=False,
+            posted_by_id=1
+            )
+
+    def test_task_creation(self):
+        task1 = Task.objects.get(title="Task 1")
+        task2 = Task.objects.get(title="Task 2")
+        task2.completed = True
+        task2.save()
+        self.assertEqual(task1.completed, False)
+        self.assertEqual(task2.completed, True)
--- taskflow/users/tests.py
+++ taskflow/users/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(
+            first_name="John",
+            last_name="Doe",
+            email="john123@example.com",
+            department=1,
+            is_boss=False
+            )
+        User.objects.create(
+            first_name="Jane",
+            last_name="Doe",
+            email="jane123@example.com",
+            department=2,
+            is_boss=True
+            )
+    
+    def test_user_creation(self):
+        john = User.objects.get(first_name="John")
+        jane = User.objects.get(first_name="Jane")
+        self.assertEqual(john.first_name, "John")
+        self.assertEqual(jane.last_name, "Doe")
