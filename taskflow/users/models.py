1c94b98c3c8913b9d3ccb06b59b289adc6bfe4bd
DavidReyesCuartas <david12033.dr@gmail.com>
2025-09-07 13:14:55 -0500

    Add is_staff field to User model and update serializers and migration

--- taskflow/users/models.py
+++ taskflow/users/models.py
@@ -18,9 +18,13 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_staff", True)
 
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser debe tener is_superuser=True.")
+        
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser debe tener is_staff=True.")
 
         return self.create_user(email, password, **extra_fields)
 
@@ -43,6 +47,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     department = models.IntegerField(choices=Department.choices)
     is_boss = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = [
